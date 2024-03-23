85c64427cd61cd8020c6e5ef0fc36d15db9ac29b
Egbertjing <junma2981@gmail.com>
2024-03-23 19:13:44 +0800

    test: test the hrtree efficiency

--- HRTree.cpp
+++ HRTree.cpp
@@ -1,6 +1,118 @@
 #include "RTreePoints.h"
 #include <map>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+using namespace std;
+#include <filesystem>
+namespace fs = filesystem;
 
+
+// summary of the points(max and min of x, y, z, time)
+std::unordered_map<std::string, float> Summary(vector<Point> points){
+    std::unordered_map<std::string, float> summary;
+    auto maxTPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.time < b.time;
+        }
+    );
+    auto minTPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.time > b.time;
+        }
+    );
+    auto maxXPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.time < b.time;
+        }
+    );
+    auto minXPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.x > b.x;
+        }
+    );
+    auto maxYPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.x < b.x;
+        }
+    );
+    auto minYPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.y > b.y;
+        }
+    );
+    auto maxZPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.z < b.z;
+        }
+    );
+    auto minZPoint = std::max_element(
+        points.begin(),
+        points.end(),
+        [](Point& a, Point& b) {
+            return a.z > b.z;
+        }
+    );
+    summary["maxTime"] = maxTPoint->time;
+    summary["minTime"] = minTPoint->time;
+    summary["maxX"] = maxXPoint->x;
+    summary["minX"] = minXPoint->x;
+    summary["maxY"] = maxYPoint->y;
+    summary["minY"] = minYPoint->y;
+    summary["maxZ"] = maxZPoint->z;
+    summary["minZ"] = minZPoint->z;
+    return summary;
+
+
+}
+
+vector<Point> GetDataFromSingleCSV(const string& filename){
+    bool firstPoint = true;
+    vector<Point> points;
+    ifstream file(filename);
+    string line;
+    if (!getline(file, line)) { // Skip header
+        cerr << "Error reading the file or the file is empty: " << filename << endl;
+        return points; // Return empty bounds if file is empty or can't be read
+    }
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string value;
+        vector<string> values;
+
+        while (getline(ss, value, ',')) {
+            values.push_back(value);
+        }
+
+        float x = stof(values[0]);
+        float y = stof(values[1]);
+        float z = stof(values[2]);
+        float time = stof(values[3]);
+        Point point = Point(x, y, z, -1, -1, -1, filename, floor(time));
+        points.push_back(point);
+    }
+ 
+    return points;
+
+
+}
 
 class RTreeNode {
 public: 
@@ -87,18 +199,21 @@ public:
 
 
 int main(){
-    vector<Point> points; 
-    Point tmp = Point(1.1, 2, 3, 1.1, 1.1, 1.1, "", 1.1);
-    points.push_back(tmp);
-    tmp = Point(1.2, 2.5, 3.1, 1.2, 1.2, 1.2, "", 1.2);
-    points.push_back(tmp);
-    tmp = Point(1.3,2.2,3.3,1.3 ,1.3, 1.3, "", 1.3);
-    points.push_back(tmp);
-    // for (Point point : points){
-    //     cout<< point.time;
-    // }
+
+    vector<Point> points = GetDataFromSingleCSV("/home/hjingaa/github/PointCloud_Octree/data_csv/whampoa_1223.csv");
+    std::unordered_map<std::string, float> map = Summary(points);
+    // vector<Point> points; 
+    // Point tmp = Point(1.1, 2, 3, 1.1, 1.1, 1.1, "", 1.1);
+    // points.push_back(tmp);
+    // tmp = Point(1.2, 2.5, 3.1, 1.2, 1.2, 1.2, "", 1.2);
+    // points.push_back(tmp);
+    // tmp = Point(1.3,2.2,3.3,1.3 ,1.3, 1.3, "", 1.3);
+    // points.push_back(tmp);
+    // // for (Point point : points){
+    // //     cout<< point.time;
+    // // }
     HRTree* tree = new HRTree(points);
-    // tree->print_tree();
+    // // tree->print_tree();
     std::vector<Point> results;
     Point min = Point(0,0,0);
     Point max = Point(15,15,15);
